530a9050a7062ff246d7a8404f6809cd183fe4a4
Nawab_AS <hamid.syed.m@gmail.com>
2025-01-27 17:33:16 -0500

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+let ground = [];
+let length = 600;
+let offset = -100
+let hillSize = 250
+
+function setup() {
+    createCanvas(800, 450);
+
+}
+
+function draw() {
+    background(18, 224, 211);
+}
