55cb4f4c7f7cb7b55059f6601428c0613858c623
Nawab_AS <hamid.syed.m@gmail.com>
2025-01-28 13:54:29 -0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let ground = [];
-let length = 600;
-let offset = -100
-let hillSize = 250
+let groundLength = 1000;
+let offset = -100;
+let hillSize = 450;
 
 function setup() {
     createCanvas(800, 450);
-
+    for (let i = 0; i < groundLength; i++) {
+        ground.push(hillSize*noise(i/250))
+    }
 }
 
 function draw() {
     background(18, 224, 211);
+    beginShape();
+    for (let i = 0; i < ground.length; i++) {
+        vertex(i, ground[i]);
+    }
+    endShape();
 }
